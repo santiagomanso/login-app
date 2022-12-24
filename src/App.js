4dca5d02a123d18b4191283f611fc0575b695332
santiagomanso <santiagomanso@web.de>
2022-12-24 18:39:40 +0100

    🔨 created app/routing/protected route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { AuthContextProvider } from './components/context/AuthContext'
 import LoginForm from './components/LoginForm'
-
+import Home from './components/Home'
+import RegisterForm from './components/RegisterForm'
+import ProtectedRoute from './components/ProtectedRoute'
 function App() {
-  return <LoginForm />
+  return (
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path='/login' element={<LoginForm />} />
+          <Route path='/register' element={<RegisterForm />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
+  )
 }
 
 export default App
